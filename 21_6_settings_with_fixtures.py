f4f61e83eff441ed8fed544c61ed2aca17d71945
ODudinova <pogorelayaoksana@gmail.com>
2022-12-14 20:16:17 +0300

    added task_21_6

---
+++ 21_6_settings_with_fixtures.py
@@ -0,0 +1,5 @@
+valid_email = 'reudacriluzo-2903@yopmail.com'
+valid_password = 'tSR3pB3StcXTa'
+invalid_email = 'qwerty@yuio.com'
+invalid_password = "12345678"
+base_url = 'https://petfriends.skillfactory.ru/'
